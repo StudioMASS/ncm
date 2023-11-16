495e5172299721a947996be9c83fab5a7672bfa3
bbeagley00 <95063436+bbeagley00@users.noreply.github.com>
2023-11-16 22:46:50 +1100

    .

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<script lang="ts">
+<script>
+	import { onMount } from 'svelte';
+
 	import Faqs from '../components/faqs.svelte';
 	import Floorplan from '../components/floorplan.svelte';
 	import Footer from '../components/footer.svelte';
@@ -7,18 +9,43 @@
 	import List from '../components/list.svelte';
 	import Location from '../components/location.svelte';
 	import Nav from '../components/nav.svelte';
-	import type { PageData } from './$types';
 
-	export let data;
+	let scrollY, ncm, building, resources, signup;
+	let ncmPerc, buildingPerc, resourcesPerc, signupPerc;
+
+	const scrollListener = () => {
+		ncmPerc = Math.round((scrollY / ncm) * 100);
+		buildingPerc = Math.round(((scrollY - ncm) / building) * 100);
+		resourcesPerc = Math.round(((scrollY - ncm - building) / resources) * 100);
+		console.log(resourcesPerc);
+	};
+
+	onMount(() => {
+		window.addEventListener('scroll', scrollListener);
+
+		return () => {
+			window.removeEventListener('scroll', scrollListener);
+		};
+	});
 </script>
 
 <section>
-	<Header />
-	<Intro />
-	<Floorplan />
-	<Faqs />
-	<Location />
-	<List />
-	<Footer />
+	<div bind:clientHeight={ncm}>
+		<Header />
+		<Intro />
+	</div>
+	<div bind:clientHeight={building}>
+		<Floorplan />
+		<Faqs />
+		<Location />
+	</div>
+	<div bind:clientHeight={resources}>
+		<List />
+	</div>
+	<div bind:clientHeight={signup}>
+		<Footer />
+	</div>
 	<Nav />
 </section>
+
+<svelte:window bind:scrollY />
